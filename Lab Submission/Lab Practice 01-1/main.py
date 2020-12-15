1f5f3e3c5312989fa019b73b0bdd121352940593
4NUBlS <xexjz0leechersteamofficialclub@gmail.com>
2020-12-16 00:37:21 +0700

    update

---
+++ Lab Submission/Lab Practice 01-1/main.py
@@ -0,0 +1,9 @@
+Number_1 = 50
+Number_2 = 35
+Summation = 0
+
+print("Hello World")
+print("This is my first program")
+
+Summation = Number_1 + Number_2
+print("{0} + {1} = {2}".format(Number_1,Number_2,Summation))
