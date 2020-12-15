7de0fd709c1eb49baa4cb186b6d6688a93db314e
4NUBlS <xexjz0leechersteamofficialclub@gmail.com>
2020-12-15 07:58:12 +0700

    update

---
+++ Class Activity/Class activity 2/main.py
@@ -0,0 +1,17 @@
+# Command 1
+print("Hello, World")
+
+# Command 2
+iPhonePrice = 30000
+gpax = 4
+print("iPhonePrice => {0}, gpax => {1:.2f}".format(iPhonePrice,gpax))
+
+# Command 3
+student_name = "4NUB!S"
+student_id = 1234567890
+print("student_name => {0}, student_id => {1}".format(student_name,student_id))
+
+# Command 4
+university_name = str(input("Input Your University Name => "))
+faculty_name = str(input("Input Your Faculty Name => "))
+print("university_name => {0}, faculty_name => {1}".format(university_name,faculty_name))
