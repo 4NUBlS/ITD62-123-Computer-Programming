67b17b9b95e44067710a924942264cb1676ff335
4NUBlS <xexjz0leechersteamofficialclub@gmail.com>
2021-01-05 17:32:55 +0700

    update

---
+++ Lab Submission/Lab Practice 06-1/main.py
@@ -0,0 +1,75 @@
+# program description
+# covid_19
+
+# print author information and date-time when executed
+from datetime import datetime  # datetime object containing current date and time
+import json
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")  # dd/mm/YY H:M:S
+print("Executed time: ", dt_string)
+name = 'xxxxxxxx'
+std_id = 'xxxxxxxx'
+lab = 'LP-06'
+print(lab + ' Name: '+name+' Student ID: ' + std_id)
+print('-----------------------------------------')
+
+# variables declaration
+
+
+# command scripts
+locations = {
+    "1": "กรุงเทพมหานคร",
+    "2": "จังหวัดกาญจนบุรี",
+    "3": "จังหวัดจันทบุรี",
+    "4": "จังหวัดฉะเชิงเทรา",
+    "5": "จังหวัดชุมพร",
+    "6": "จังหวัดชลบุรี",
+    "7": "จังหวัดตราด",
+    "8": "จังหวัดตาก",
+    "9": "จังหวัดนครนายก",
+    "10": "จังหวัดนครปฐม",
+    "11": "จังหวัดนนทบุรี",
+    "12": "จังหวัดปทุมธานี",
+    "13": "จังหวัดประจวบคีรีขันธ์",
+    "14": "จังหวัดปราจีนบุรี",
+    "15": "จังหวัดพระนครศรีอยุธยา",
+    "16": "จังหวัดเพชรบุรี",
+    "17": "จังหวัดราชบุรี",
+    "18": "จังหวัดระนอง",
+    "19": "จังหวัดระยอง",
+    "20": "จังหวัดลพบุรี",
+    "21": "จังหวัดสิงห์บุรี",
+    "22": "จังหวัดสมุทรปราการ",
+    "23": "จังหวัดสมุทรสงคราม",
+    "24": "จังหวัดสมุทรสาคร",
+    "25": "จังหวัดสุพรรณบุรี",
+    "26": "จังหวัดสระแก้ว",
+    "27": "จังหวัดสระบุรี",
+    "28": "จังหวัดอ่างทอง",
+    "0": "อื่นๆ"
+}
+
+persondict = {
+    "datatime": dt_string,
+    "fname": str(input("Enter first name: ")),
+    "lname": str(input("Enter last name: ")),
+    "age": int(input("Enter age: ")),
+    "body_temp": float(input("Enter body temperature: ")),
+    "location": "",
+    "risk": False,
+    "covid": False
+}
+
+print("Location")
+for x, y in locations.items():
+    print("{0}. {1}".format(x, y))
+persondict.update({"location": int(input("Enter location number: "))})
+if persondict.get("location") == 0:
+    persondict.update({"risk": False})
+elif str(persondict.get("location")) in locations.keys():
+    persondict.update({"risk": True})
+if persondict.get("body_temp") > 37.5 and str(persondict.get("location")) in locations.keys():
+    persondict.update({"covid": True})
+elif persondict.get("body_temp") > 37.5:
+    persondict.update({"risk": True})
+print("JSON={0}".format(json.dumps(persondict)))
